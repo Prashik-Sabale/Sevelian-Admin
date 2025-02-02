f5a8a393f707b93925e68d8b7c5ec4e8c974ee1d
Prashik Sabale <blackdarkstar71@gmail.com>
2025-02-02 19:48:28 +0530

    update

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -6,10 +6,10 @@ const Dashboard = () => {
         <div>
             <h2 className="text-xl font-bold mb-6">Dashboard Overview</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-                <OverviewCard title="Total Products" value="120" color="bg-green-500" />
+                <OverviewCard title="Total Products" value="12" color="bg-green-500" />
                 <OverviewCard title="Total Orders" value="1,230" color="bg-blue-500" />
-                <OverviewCard title="Total Revenue" value="$45,000" color="bg-yellow-500" />
-                <OverviewCard title="Users" value="980" color="bg-purple-500" />
+                <OverviewCard title="Total Revenue" value="₹40,000" color="bg-yellow-500" />
+                <OverviewCard title="Users" value="800" color="bg-purple-500" />
             </div>
         </div>
     );
